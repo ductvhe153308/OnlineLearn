b228ed5d18f6faa2068bd506a2edbde60ec00aef
nguyennqhe151209 <94712373+nguyennqhe151209@users.noreply.github.com>
2022-06-23 22:30:46 +0700

    QuizDAO
    QuizDAO

--- src/java/dal/QuizDAO.java
+++ src/java/dal/QuizDAO.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import model.Choice;
 import model.Question;
 import utils.DBContext;
 
@@ -59,5 +60,76 @@ public class QuizDAO {
             }
         }
         return questions;
+    }
+    public List<Question> getTenRandomQuestions(int id) throws ClassNotFoundException {
+        List<Question> questions = new ArrayList<>();
+        
+        try {
+            this.conn = new DBContext().getConnection();
+            ps = conn.prepareStatement("" +
+                    "SELECT qu.Id AS QuestionId, qu.Description AS QuestionDescription " +
+                    "FROM quiztype AS qt INNER JOIN question AS qu ON qt.Id=Fk_QuizTypeId_Question " +
+                    "WHERE qt.Id=? " +
+                    "ORDER BY RAND() LIMIT 10");
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+            int count = 1;
+            while (rs.next()) {
+               
+                Question qu = new Question();
+                qu.setQuestionId(rs.getInt("QuestionId"));
+                qu.setId(count);
+                qu.setQuestion(rs.getString("QuestionDescription"));
+                List<Choice> choices = getChoices(rs.getInt("QuestionId"));
+                for (int i = 0; i <choices.size(); i++) {
+                    if (i == 0) qu.getOptions().get(0).replace("a", choices.get(i).getDescription());
+                    if (i == 1) qu.getOptions().get(0).replace("b", choices.get(i).getDescription());
+                    if (i == 2) qu.getOptions().get(0).replace("c", choices.get(i).getDescription());
+                    if (i == 3) qu.getOptions().get(0).replace("d", choices.get(i).getDescription());
+                    if (choices.get(i).getIsAnswer() == 1) qu.setAnswer(choices.get(i).getDescription());
+                }
+                questions.add(qu);
+                count++;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (ps != null) ps.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return questions;
+    }
+    public List<Choice> getChoices(int id) throws ClassNotFoundException  {
+        List<Choice> choices = new ArrayList<>();
+        
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement("SELECT c.Id AS ChoiceId, c.Description AS ChoiceDescription, c.IsAnswer AS ChoiceIsAnswer FROM quizdb.choice AS c INNER JOIN quizdb.question AS qu ON qu.Id=c.Fk_QuestionId_Choice  where qu.id = ;");
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                Choice c = new Choice();
+                c.setChoiceId(rs.getInt("ChoiceId"));
+                c.setDescription(rs.getString("ChoiceDescription"));
+                c.setIsAnswer(rs.getInt("ChoiceIsAnswer"));
+                choices.add(c);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            try {
+                if (ps != null) ps.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return choices;
     }
 }
