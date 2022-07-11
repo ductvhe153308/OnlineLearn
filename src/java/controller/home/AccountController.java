ce5ca6ee8eeb9c6844c2f04d52b7d107b0006be5
anntdhe140298 <77199495+trongan1907@users.noreply.github.com>
2022-07-11 23:21:17 +0700

    Update AccountController.java

--- src/java/controller/home/AccountController.java
+++ src/java/controller/home/AccountController.java
@@ -20,7 +20,6 @@ import model.Account;
  *
  * @author admin
  */
-
 public class AccountController extends HttpServlet {
 
     /**
@@ -73,11 +72,15 @@ public class AccountController extends HttpServlet {
                 request.getRequestDispatcher("login.jsp").forward(request, response);
             }
             if (a != null) {
-                HttpSession session = request.getSession();
-                session.setAttribute("user", a);
-                session.setAttribute("email", email);
-                session.setAttribute("password", password);
-                response.sendRedirect("home.jsp");
+                if (a.getRole_id() == 1) {
+                    response.sendRedirect("admin/dashboard");
+                } else {
+                    HttpSession session = request.getSession();
+                    session.setAttribute("user", a);
+                    session.setAttribute("email", email);
+                    session.setAttribute("password", password);
+                    response.sendRedirect("home.jsp");
+                }
             }
         } catch (Exception e) {
         }
