f5376866a97c65cb42ee07716e9d3c3a42d483a3
anntdhe140298 <77199495+trongan1907@users.noreply.github.com>
2022-07-11 10:52:45 +0700

    Update BlogDetailController.java

--- src/java/controller/home/BlogDetailController.java
+++ src/java/controller/home/BlogDetailController.java
@@ -31,7 +31,6 @@ public class BlogDetailController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
         try {
             int id = Integer.parseInt(request.getParameter("id"));
             BlogDAO blogDAO = new BlogDAO();
