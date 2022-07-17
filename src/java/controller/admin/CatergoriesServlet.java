1ff46edb765a6685e5a8fd90ccca9bd62304ea5b
ductvhe153308 <ductvhe153308@fpt.edu.vn>
2022-07-17 13:47:16 +0700

    controller category

--- src/java/controller/admin/CatergoriesServlet.java
+++ src/java/controller/admin/CatergoriesServlet.java
@@ -5,15 +5,22 @@
  */
 package controller.admin;
 
+import com.google.gson.Gson;
 import dal.AccountDAO;
+import dal.CategoryDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Account;
+import model.Category;
 
 /**
  *
@@ -22,21 +29,6 @@ import model.Account;
 @WebServlet(name = "CatergoriesServlet", urlPatterns = {"/admin/categories"})
 public class CatergoriesServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        request.getRequestDispatcher("categories.jsp").forward(request, response);
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -48,7 +40,7 @@ public class CatergoriesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        request.getRequestDispatcher("categories.jsp").forward(request, response);
     }
 
     /**
@@ -62,7 +54,14 @@ public class CatergoriesServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            CategoryDAO  cdao = new CategoryDAO();
+            ArrayList<Category> cs = cdao.getListCategories(1, 10);
+            Gson gson = new Gson();
+            response.getWriter().print(gson.toJson(cs));
+        } catch (SQLException ex) {
+            Logger.getLogger(CatergoriesServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -73,6 +72,6 @@ public class CatergoriesServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
 }
