d61eb9baf094a952869f43e4255ccea66c8c57b6
nguyennqhe151209 <94712373+nguyennqhe151209@users.noreply.github.com>
2022-07-17 12:37:49 +0700

    update choice model

--- src/java/model/Choice.java
+++ src/java/model/Choice.java
@@ -43,7 +43,7 @@ public class Choice {
     @Override
     public String toString() {
         return "Choice{" +
-                "choiceId=" + choiceId +
+                "\nchoiceId=" + choiceId +
                 ", description='" + description + '\'' +
                 ", isAnswer=" + isCorrectAnswer +
                 '}';
