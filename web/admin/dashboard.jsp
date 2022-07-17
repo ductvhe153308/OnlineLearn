7a6f8c008023d2c17af534eaaf90b6ffd88b4051
ductvhe153308 <ductvhe153308@fpt.edu.vn>
2022-07-17 15:09:34 +0700

    Update front-end

--- web/admin/dashboard.jsp
+++ web/admin/dashboard.jsp
@@ -19,9 +19,95 @@
         <jsp:include page="static/layout/header.jsp" />
         <jsp:include page="static/layout/slide-bar.jsp" />
         <div id="page">
-            <!--html của #page-->
+            <div class="row">
+                <div class="col col-4">
+                    <div class="row">
+                        <div class="col-12 component" id="profile"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 component">
+                            <div class="component-header">Invoice Analytics</div>
+                            <canvas id="invoice"></canvas>
+                        </div>
+                    </div>
+                </div>
+                <div class="col col-8">
+                    <div class="row">
+                        <div class="col-4 component" id="members">
+                            <div class="row">
+                                <div class="col-3 circle bg-blue">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#009da6" class="bi bi-person-fill" viewBox="0 0 16 16"><path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>
+                                </div>
+                                <div class="col-3 big-num">
+                                    166
+                                </div>
+                            </div>
+                            <div class="row text-mute">
+                                Members
+                            </div>
+                            <div class="row">
+                                <div class="progress">
+                                    <div class="progress-filler bg-st-blue" style="width : 54%"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4 component" id="appointments">
+                            <div class="row">
+                                <div class="col-3 circle bg-green">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#3dd598" class="bi bi-credit-card-fill" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0V4zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7H0zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1z"/></svg>
+                                </div>
+                                <div class="col-3 big-num">
+                                    321
+                                </div>
+                            </div>
+                            <div class="row text-mute">
+                                Appointments
+                            </div>
+                            <div class="row">
+                                <div class="progress">
+                                    <div class="progress-filler bg-st-green" style="width : 34%"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-4 component" id="mentoringpoints">
+                            <div class="row">
+                                <div class="col-3 circle bg-orange">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#f39c12" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>                                </div>
+                                <div class="col-3 big-num">
+                                    224
+                                </div>
+                            </div>
+                            <div class="row text-mute">
+                                Mentoring Points
+                            </div>
+                            <div class="row">
+                                <div class="progress">
+                                    <div class="progress-filler bg-st-orange" style="width : 64%"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12 component">
+                            <div class="component-header">Sales Analytics</div>
+                            <canvas id="sale"></canvas>
+                        </div>  
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6 component" id="mentor">
+                </div>
+                <div class="col-6 component" id="mentee">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 component" id="course">
+                </div>
+            </div>
         </div>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <!-- Library for build chart!-->
         <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <script src="static/js/dashboard.js"></script>
         <script src="static/js/mentor.js"></script>
--- web/admin/static/js/dashboard.js
+++ web/admin/static/js/dashboard.js
@@ -4,111 +4,24 @@
  * and open the template in the editor.
  */
 
-var Dashboard = new function __Dashboard(){
+var Dashboard = new function __Dashboard() {
 
-/**
- * Master layout for dashboard.
- */
-this.init = function () {
-$('#page').html(`
-            <div class="row">
-                <div class="col col-4">
-                    <div class="row">
-                        <div class="col-12 component" id="profile"></div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 component">
-                            <div class="component-header">Invoice Analytics</div>
-                            <canvas id="invoice"></canvas>
-                        </div>
-                    </div>
-                </div>
-                <div class="col col-8">
-                    <div class="row">
-                        <div class="col-4 component" id="members">
-                            <div class="row">
-                                <div class="col-3 circle bg-blue">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#009da6" class="bi bi-person-fill" viewBox="0 0 16 16"><path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/></svg>
-                                </div>
-                                <div class="col-3 big-num">
-                                    166
-                                </div>
-                            </div>
-                            <div class="row text-mute">
-                                Members
-                            </div>
-                            <div class="row">
-                                <div class="progress">
-                                    <div class="progress-filler bg-st-blue" style="width : 54%"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4 component" id="appointments">
-                            <div class="row">
-                                <div class="col-3 circle bg-green">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#3dd598" class="bi bi-credit-card-fill" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v1H0V4zm0 3v5a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7H0zm3 2h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-1a1 1 0 0 1 1-1z"/></svg>
-                                </div>
-                                <div class="col-3 big-num">
-                                    321
-                                </div>
-                            </div>
-                            <div class="row text-mute">
-                                Appointments
-                            </div>
-                            <div class="row">
-                                <div class="progress">
-                                    <div class="progress-filler bg-st-green" style="width : 34%"></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-4 component" id="mentoringpoints">
-                            <div class="row">
-                                <div class="col-3 circle bg-orange">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#f39c12" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>                                </div>
-                                <div class="col-3 big-num">
-                                    224
-                                </div>
-                            </div>
-                            <div class="row text-mute">
-                                Mentoring Points
-                            </div>
-                            <div class="row">
-                                <div class="progress">
-                                    <div class="progress-filler bg-st-orange" style="width : 64%"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12 component">
-                            <div class="component-header">Sales Analytics</div>
-                            <canvas id="sale"></canvas>
-                        </div>  
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-6 component" id="mentor">
-                </div>
-                <div class="col-6 component" id="mentee">
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 component" id="course">
-                </div>
-            </div>`)
+    /**
+     * Master layout for dashboard.
+     */
+    this.init = function () {
         this.profile()
         this.sale()
         this.invoice()
         Mentor.board.dashboard('mentor', 1, 5)
         Mentee.board.dashboard('mentee', 1, 5)
         Course.board.dashboard('course', 1, 5)
-        }
-/**
- * Admin profile
- */
-this.profile = function () {
-var profile = $("#profile")
+    };
+    /**
+     * Admin profile
+     */
+    this.profile = function () {
+        var profile = $("#profile");
         profile.html(`
                 <div class="row profile-header">
                     <div class="col-7">
@@ -139,124 +52,125 @@ var profile = $("#profile")
                 </div>
             `)
         $.ajax({type: 'POST',
-                url: '../ajax/admin/profile',
-                data: {
-                },
-                dataType: 'json',
-                success: function (response) {
+            url: '../ajax/admin/profile',
+            data: {
+            },
+            dataType: 'json',
+            success: function (response) {
                 var admin = response;
-                        $('#admin-ava img').attr('src', `/onlinelearn/assets/img/user/${admin.pfp}`);
-                        $("#admin-name").html(admin.last_name + " " + admin.first_name);
-                },
-                error: function (XHR) {
+                $('#admin-ava img').attr('src', `/onlinelearn/assets/img/user/${admin.pfp}`);
+                $("#admin-name").html(admin.last_name + " " + admin.first_name);
+            },
+            error: function (XHR) {
 
-                }
+            }
         })
-}
-/**
- * Invoice Chart 
- */
-this.invoice = function () {
-$.ajax({
-type: 'post',
-        url: '../ajax/admin/invoice',
-        data:{
+    };
 
-        },
-        success: function (response) {
-        var config = {
-        type: 'doughnut',
-                data:  {
-                labels: [
-                        'Invoiced',
-                        'Received',
-                        'Pending'
-                ],
+    /**
+     * Invoice Chart 
+     */
+    this.invoice = function () {
+        $.ajax({
+            type: 'post',
+            url: '../ajax/admin/invoice',
+            data: {
+
+            },
+            success: function (response) {
+                var config = {
+                    type: 'doughnut',
+                    data: {
+                        labels: [
+                            'Invoiced',
+                            'Received',
+                            'Pending'
+                        ],
                         datasets: [
-                        {
-                        label: 'My First Dataset',
+                            {
+                                label: 'My First Dataset',
                                 data: JSON.parse(response),
                                 backgroundColor: [
-                                        'rgb(255, 99, 132)',
-                                        'rgb(54, 162, 235)',
-                                        'rgb(255, 205, 86)'
+                                    'rgb(255, 99, 132)',
+                                    'rgb(54, 162, 235)',
+                                    'rgb(255, 205, 86)'
                                 ],
                                 hoverOffset: 3
-                        }]
+                            }]
+                    }
                 }
-        }
-        var myChart = new Chart($('#invoice'), config)
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-        }
-})
-        }
-/**
- * Sale Chart
- */
-this.sale = function () {
-$.ajax({
-type: 'post',
-        url: '../ajax/admin/sale',
-        data:{
+                var myChart = new Chart($('#invoice'), config)
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+            }
+        })
+    };
+    /**
+     * Sale Chart
+     */
+    this.sale = function () {
+        $.ajax({
+            type: 'post',
+            url: '../ajax/admin/sale',
+            data: {
 
-        },
-        success: function (response) {
-        console.log(response)
+            },
+            success: function (response) {
+                console.log(response)
                 var data = JSON.parse(response)
                 console.log(data)
                 var labels = JSON.parse(data.labels)
                 var rec = JSON.parse(data.receiveds)
                 var pen = JSON.parse(data.pendings)
                 var config = {
-                type: 'bar',
-                        data:  {
+                    type: 'bar',
+                    data: {
                         labels: labels,
-                                datasets: [
-                                {
+                        datasets: [
+                            {
                                 label: 'Received',
-                                        data: rec,
-                                        backgroundColor: [
-                                                'rgb(0, 157, 166)'
-                                        ],
-                                        borderWidth: 0,
-                                        borderRadius: 50,
-                                        hoverOffset: 3
-                                }, {
+                                data: rec,
+                                backgroundColor: [
+                                    'rgb(0, 157, 166)'
+                                ],
+                                borderWidth: 0,
+                                borderRadius: 50,
+                                hoverOffset: 3
+                            }, {
                                 label: 'Pending',
-                                        data: pen,
-                                        backgroundColor: [
-                                                'rgb(255, 156, 39)'
-                                        ],
-                                        borderWidth: 0,
-                                        borderRadius: 50,
-                                        hoverOffset: 3
-                                }]
-                        },
-                        options: {
+                                data: pen,
+                                backgroundColor: [
+                                    'rgb(255, 156, 39)'
+                                ],
+                                borderWidth: 0,
+                                borderRadius: 50,
+                                hoverOffset: 3
+                            }]
+                    },
+                    options: {
                         scales: {
-                        y: {
-                        beginAtZero: true
+                            y: {
+                                beginAtZero: true
+                            }
                         }
-                        }
-                        }
+                    }
                 };
                 var myChart = new Chart($('#sale'), config)
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-        }
-})
-}
-/**
- * Booking List
- */
-this.course = function () {
-var table_name = 'Course List';
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+            }
+        })
+    };
+    /**
+     * Booking List
+     */
+    this.course = function () {
+        var table_name = 'Course List';
         var header = ['Mentor Name', 'Course', 'Mentee Name', 'Booking Time', 'Status', 'Amount']
         var colgroup = [100, 60, 90, 70, 50, 70]
         var data = [1, 2, 3, 4, 5];
         var table = AP.table.render(table_name, header, data, function (index) {
-        return `<div class="table-row">
+            return `<div class="table-row">
                         <div class="table-data" style="width:${colgroup[0]}px">${index}</div>
                         <div class="table-data" style="width:${colgroup[1]}px">${index}</div>
                         <div class="table-data" style="width:${colgroup[2]}px">${index}</div>
@@ -266,5 +180,5 @@ var table_name = 'Course List';
                     </div>`
         }, colgroup)
         $("#page #course").html(table)
-        }
+    };
 }
