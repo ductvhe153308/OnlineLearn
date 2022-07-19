46c27bd9622e31518507fcbcd1895a325c25caf5
hduongg09 <hoangduong.l624@gmail.com>
2022-07-19 22:15:12 +0700

    Update MentorDetailController.java

--- src/java/controller/mentor/MentorDetailController.java
+++ src/java/controller/mentor/MentorDetailController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.home;
+package controller.mentor;
 
 import dal.AccountDAO;
 import dal.BlogDAO;
