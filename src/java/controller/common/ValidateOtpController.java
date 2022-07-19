c9ec2839926eb2219ebf0fd787fe68eea7fe4ecc
hduongg09 <hoangduong.l624@gmail.com>
2022-07-19 22:21:27 +0700

    Update ValidateOtpController.java

--- src/java/controller/common/ValidateOtpController.java
+++ src/java/controller/common/ValidateOtpController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.home;
+package controller.common;
 
 import java.io.IOException;
 import java.io.PrintWriter;
