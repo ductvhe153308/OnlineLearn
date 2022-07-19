359d268c352656512ea62c7cbd0fad11700cb8e9
anntdhe140298 <77199495+trongan1907@users.noreply.github.com>
2022-07-19 21:39:18 +0700

    Create SearchBlogController.java

---
+++ src/java/controller/home/SearchBlogController.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.home;
+
+import dal.BlogDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Blog;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class SearchBlogController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String searchBlog = request.getParameter("searchBlog");
+        if (searchBlog.length() != 0) {
+            String index = request.getParameter("index");
+            if (index == null) {
+                index = "1";
+            }
+            int indexPage = Integer.parseInt(index);
+            BlogDAO dao = new BlogDAO();
+            int endPage = dao.getNumberPageBlogBySearch(searchBlog);
+            List<Blog> list = dao.getBlogBySearchName(indexPage, searchBlog);
+            request.setAttribute("list", list);
+            request.setAttribute("endP", endPage);
+            request.setAttribute("tag", indexPage);
+            request.setAttribute("searchBlog", searchBlog);
+            request.getRequestDispatcher("blog-list.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("BlogList");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
