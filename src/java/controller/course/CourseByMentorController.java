7f6aeb69e5fc9506b8340d508a50c8d97483c039
hduongg09 <hoangduong.l624@gmail.com>
2022-07-19 22:06:32 +0700

    Update CourseByMentorController.java

--- src/java/controller/course/CourseByMentorController.java
+++ src/java/controller/course/CourseByMentorController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.home;
+package controller.course;
 
 import dal.CourseDAO;
 import java.io.IOException;
