aba9b64a04bee48894ac9328342656d718ab6c68
ductvhe153308 <106256580+ductvhe153308@users.noreply.github.com>
2022-07-20 10:00:25 +0700

    Update AddCategoryServlet.java

--- src/java/controller/admin/AddCategoryServlet.java
+++ src/java/controller/admin/AddCategoryServlet.java
@@ -60,14 +60,4 @@ public class AddCategoryServlet extends HttpServlet {
         response.sendRedirect("../categories");
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-
 }
