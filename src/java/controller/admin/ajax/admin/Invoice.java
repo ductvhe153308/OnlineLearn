2ad043c264a9e0ad4be823789ba6fb414bfdbc3d
ductvhe153308 <106256580+ductvhe153308@users.noreply.github.com>
2022-07-20 10:04:45 +0700

    Update Invoice.java

--- src/java/controller/admin/ajax/admin/Invoice.java
+++ src/java/controller/admin/ajax/admin/Invoice.java
@@ -20,34 +20,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "Invoice", urlPatterns = {"/ajax/admin/invoice"})
 public class Invoice extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.getWriter().print("[350, 20, 100]");
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -60,17 +32,6 @@ public class Invoice extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        response.getWriter().print("[350, 20, 100]");
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
