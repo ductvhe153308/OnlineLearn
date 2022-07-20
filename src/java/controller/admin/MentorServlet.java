73ff815e2518ee4a07de6eb16a2544eb1480ce85
ductvhe153308 <106256580+ductvhe153308@users.noreply.github.com>
2022-07-20 10:03:13 +0700

    Update MentorServlet.java

--- src/java/controller/admin/MentorServlet.java
+++ src/java/controller/admin/MentorServlet.java
@@ -60,15 +60,4 @@ public class MentorServlet extends HttpServlet {
             Logger.getLogger(MentorServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-
 }
