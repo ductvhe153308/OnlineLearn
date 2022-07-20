736d2e68e8ece8db137319c18b135e5b0ac9b487
ductvhe153308 <106256580+ductvhe153308@users.noreply.github.com>
2022-07-20 10:02:28 +0700

    Update DashboardServlet.java

--- src/java/controller/admin/DashboardServlet.java
+++ src/java/controller/admin/DashboardServlet.java
@@ -77,16 +77,6 @@ public class DashboardServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
     }// </editor-fold>
 
 }
