773b314e122693e1f52394fdd73c1c479adc0c85
ductvhe153308 <106256580+ductvhe153308@users.noreply.github.com>
2022-07-20 10:01:43 +0700

    Update CatergoriesServlet.java

--- src/java/controller/admin/CatergoriesServlet.java
+++ src/java/controller/admin/CatergoriesServlet.java
@@ -65,14 +65,4 @@ public class CatergoriesServlet extends HttpServlet {
         }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
-
 }
