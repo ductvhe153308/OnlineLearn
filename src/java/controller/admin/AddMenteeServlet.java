e30e281b839087f697dd45134cbc134013d071e5
ductvhe153308 <106256580+ductvhe153308@users.noreply.github.com>
2022-07-20 10:01:00 +0700

    Update AddMenteeServlet.java

--- src/java/controller/admin/AddMenteeServlet.java
+++ src/java/controller/admin/AddMenteeServlet.java
@@ -68,14 +68,4 @@ public class AddMenteeServlet extends HttpServlet {
         }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
