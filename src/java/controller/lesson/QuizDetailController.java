23ce10e877310669d2578aff31903eb12e5988f0
nguyennqhe151209 <94712373+nguyennqhe151209@users.noreply.github.com>
2022-07-21 17:40:39 +0700

    Update QuizDetailController.java

--- src/java/controller/lesson/QuizDetailController.java
+++ src/java/controller/lesson/QuizDetailController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controller.home;
+package controller.lesson;
 
 import dal.QuizDAO;
 import java.io.IOException;
